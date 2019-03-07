7b71dc6a3409ecd17fdf1ce0446d32630b89b8c0
kacperbrezden <kacperbrezden@gmail.com>
2019-03-07 16:28:54 +0100

    Storybook addons

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -2,3 +2,5 @@ import '@storybook/addon-actions/register';
 import '@storybook/addon-links/register';
 import '@storybook/addon-knobs/register';
 import 'storybook-styled-components/register'
+import '@storybook/addon-viewport/register';
+import '@storybook/addon-backgrounds/register';
--- .storybook/config.js
+++ .storybook/config.js
@@ -1,24 +1,72 @@
 import { addDecorator, configure, addParameters } from "@storybook/react";
-import { themes } from '@storybook/theming';
-import {DefaultTheme,DarkTheme} from "../src/presets";
-import { withThemes } from 'storybook-styled-components'
+import { themes } from "@storybook/theming";
+import { DefaultTheme, DarkTheme } from "../src/presets";
+import { withThemes } from "storybook-styled-components";
 
 const req = require.context("../src/components", true, /\.stories\.js$/);
 
 const componentThemes = {
-  'Default theme': DefaultTheme,
-  'Dark Theme': DarkTheme,
-}
+  "Default theme": DefaultTheme,
+  "Dark Theme": DarkTheme
+};
+const newViewports = {
+  iphone5: {
+    name: "Iphone 5/SE",
+    styles: {
+      width: "320px",
+      height: "568px"
+    }
+  },
+  iphone6: {
+    name: "Iphone 6/7/8",
+    styles: {
+      width: "375px",
+      height: "667px"
+    }
+  },
+  iphoneX: {
+    name: "Iphone X",
+    styles: {
+      width: "375px",
+      height: "812px"
+    }
+  },
+  ipad: {
+    name: "Ipad",
+    styles: {
+      width: "768px",
+      height: "1024px"
+    }
+  },
+  ipadPro: {
+    name: "Ipad Pro",
+    styles: {
+      width: "1024px",
+      height: "1366px"
+    }
+  }
+};
 
+addParameters({
+  viewport: {
+    viewports: newViewports,
+  },
+  backgrounds: [
+    { name: "white", value: "#ffffff"},
+    { name: "black", value: "#000000"},
+    { name: "twitter", value: "#00aced"},
+    { name: "facebook", value: "#3b5998" }
+  ]
+});
 // now add the decorator
 addDecorator(withThemes(componentThemes));
 
 // Option defaults.
 addParameters({
   options: {
-    name: 'Rebzden',
-    theme: themes.dark,
-  },
+    name: "Rebzden",
+    theme: themes.dark
+  }
 });
 // done!
 function loadStories() {
